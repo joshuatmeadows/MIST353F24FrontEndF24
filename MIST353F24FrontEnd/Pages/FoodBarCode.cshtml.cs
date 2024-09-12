0ebbe74cea91eef81e395b4c52f02b037e698829
Joshua Meadows <55264026+joshuatmeadows@users.noreply.github.com>
2024-09-12 15:14:38 -0400

    Add Food search

---
+++ MIST353F24FrontEnd/Pages/FoodBarCode.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST353F24FrontEnd.Pages
+{
+    public class FoodBarCodeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MIST353F24FrontEnd/wwwroot/js/site.js
+++ MIST353F24FrontEnd/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+async function fetchInfo() {
+    let userInput = document.getElementById("inputFld").value;
+    let url = `https://world.openfoodfacts.org/api/v3/product/${userInput}.json`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    let productNutrientLevels = data.product.nutrient_levels
+
+    let text = '<table class="table table-striped"><tr><th>Key</th><th>Value</th></tr>'
+
+    for (let key in productNutrientLevels) {
+        text += `<tr><td>${key}</td><td>${productNutrientLevels[key]}</td></tr>`
+    }
+
+    text += '</table>'
+
+    document.getElementById("output").innerHTML = text;
+}
+
+
